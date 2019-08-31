bb3daabc8d76f13efaff62a139d286409fb4d8c9
doanhoangvu98 <doanhuuanh4@gmail.com>
2019-08-31 14:28:57 +0700

    Add Discussion scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190831063859) do
+ActiveRecord::Schema.define(version: 20190831071150) do
+
+  create_table "discussions", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
